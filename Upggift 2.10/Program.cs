ac8d2566b3d737b230dfe428cecac97e3cb6ea57
EmilWallett <nhlpool27@gmail.com>
2022-09-07 11:49:54 +0200

    Add project files.

---
+++ Upggift 2.10/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Uppgift_210
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilken bok är din favoritbok?");
+            Console.Write("Skriv här: "); string svar = Console.ReadLine();
+            Console.WriteLine("Din favoritbok är alltså " + svar + ".");
+        }
+    }
+}
